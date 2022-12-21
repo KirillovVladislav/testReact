e9bb577216b13bf2c0aa4a624ed5167fbf6f12de
KirillovVladislav <js.kirillov@yandex.ru>
2022-12-21 22:06:01 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import { useEffect, useState } from "react";
+
+function randomInteger(min, max) {
+  let rand = min + Math.random() * (max + 1 - min);
+
+  return Math.floor(rand);
+}
+
+export default function App() {
+  const [list, setList] = useState([
+    { id: Date.now(), number: 10 },
+    { id: Date.now(), number: 15 },
+  ]);
+
+  const addNewItem = () => {
+    setList([...list, { id: Date.now(), number: randomInteger(10, 30) }]);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setList((prev) =>
+        prev
+          .filter((el) => el.number > 0)
+          .map((item) => {
+            return {
+              ...item,
+              number: item.number - 1,
+            };
+          })
+      );
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [list]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button onClick={addNewItem}>добавить</button>
+      <ul>
+        {list.map((el, index) => (
+          <li key={el.id}>
+            {index + 1}. Исчезнет через {el.number}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
-
-export default App;
